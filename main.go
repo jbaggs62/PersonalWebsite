eb9ac91ade1638633220dff6dd02b8ebe5364af9
Jacob Baggs <jacob.baggs@outlook.com>
2023-11-14 20:49:37 -0500

    testing workflows

--- main.go
+++ main.go
@@ -11,11 +11,11 @@ import (
 func main() {
 
 	componentHello := templates.Hello("Jacob")
-	componentProjects := templates.Project("Jacob")
-
+	componentProjects := templates.Projects("Jacob")
+	componentContact := templates.Contact("Jacob")
 	http.Handle("/", templ.Handler(componentHello))
 	http.Handle("/projects", templ.Handler(componentProjects))
-	http.Handle("/contact", templ.Handler(componentProjects))
+	http.Handle("/contact", templ.Handler(componentContact))
 
 	fmt.Println("Listening on :3000")
 	http.ListenAndServe(":3000", nil)
