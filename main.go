7912aec979f3e6c32406abcc8c4a7ddfeda7b8c9
Jacob Baggs <jacob.baggs@outlook.com>
2023-11-17 15:59:33 -0500

    adding air

--- main.go
+++ main.go
@@ -9,14 +9,16 @@ import (
 )
 
 func main() {
-
+	// componets for different pages
 	componentHello := templates.Hello("Jacob")
 	componentProjects := templates.Projects("Jacob")
 	componentContact := templates.Contact("Jacob")
+
+	// routes for different pages
 	http.Handle("/", templ.Handler(componentHello))
 	http.Handle("/projects", templ.Handler(componentProjects))
 	http.Handle("/contact", templ.Handler(componentContact))
 
-	fmt.Println("Listening on :3000")
-	http.ListenAndServe(":3000", nil)
+	fmt.Println("Listening on :8080")
+	http.ListenAndServe(":8080", nil)
 }
